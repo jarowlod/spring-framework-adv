d0b6c8e338715c74c482af7c9bd2e48480745008
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-28 14:29:51 +0100

    Clean

--- src/main/java/pl/training/shop/payments/adapters/persistence/JpaPaymentRepositoryMapper.java
+++ src/main/java/pl/training/shop/payments/adapters/persistence/JpaPaymentRepositoryMapper.java
@@ -11,7 +11,7 @@ import pl.training.shop.payments.domain.PaymentStatusDomain;
 @Mapper(componentModel = "spring", imports = {java.math.BigDecimal.class, org.javamoney.moneta.Money.class})
 public interface JpaPaymentRepositoryMapper {
 
-    @Mapping(target = "value", expression = "java(BigDecimal.valueOf(paymentDomain.getValue().getNumber().doubleValueExact()))")
+    @Mapping(target = "value", expression = "java(paymentDomain.getValue().getNumberStripped())")
     @Mapping(target = "currency", expression = "java(paymentDomain.getValue().getCurrency().getCurrencyCode())")
     PaymentEntity toEntity(PaymentDomain paymentDomain);
 
