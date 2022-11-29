4fcb506c4f4a7a97834614431fc56f5adb686a88
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-29 17:02:33 +0100

    kk

--- src/main/java/pl/training/shop/commons/Annotations.java
+++ src/main/java/pl/training/shop/commons/Annotations.java
@@ -1,28 +1,34 @@
 package pl.training.shop.commons;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Optional;
 
 public class Annotations {
 
-    public static  <T extends Annotation> T getClassAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) {
+    public static <T extends Annotation> T getClassAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) {
         return joinPoint.getTarget().getClass().getAnnotation(type);
     }
 
-    public static  <T extends Annotation> T getMethodAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) throws NoSuchMethodException {
+    public static <T extends Annotation> T getMethodAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) throws NoSuchMethodException {
+        return getTargetMethod(joinPoint).getAnnotation(type);
+    }
+
+    public static <T extends Annotation> T findAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) throws NoSuchMethodException {
+        var annotation = getMethodAnnotation(joinPoint, type);
+        return annotation != null ? annotation : getClassAnnotation(joinPoint, type);
+    }
+
+    public static Method getTargetMethod(JoinPoint joinPoint) throws NoSuchMethodException {
         var signature = (MethodSignature) joinPoint.getSignature();
         var methodName = signature.getMethod().getName();
         var parameterTypes = signature.getMethod().getParameterTypes();
-        return joinPoint.getTarget().getClass().getMethod(methodName, parameterTypes).getAnnotation(type);
-    }
-
-    public static  <T extends Annotation> T findAnnotation(ProceedingJoinPoint joinPoint, Class<T> type) throws NoSuchMethodException {
-        var annotation = getClassAnnotation(joinPoint, type);
-        return annotation != null ? annotation : getMethodAnnotation(joinPoint, type);
+        return joinPoint.getTarget().getClass().getMethod(methodName, parameterTypes);
     }
 
     public static <T extends Annotation> Optional<T> findAnnotation(Annotation[] annotations, Class<T> type) {
@@ -30,6 +36,23 @@ public class Annotations {
                 .filter(type::isInstance)
                 .map(type::cast)
                 .findFirst();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static  <P, A extends Annotation> void taskForArgument(JoinPoint joinPoint, Class<A> annotationType, Task<P, A> task) throws NoSuchMethodException {
+        var arguments = joinPoint.getArgs();
+        var argumentsAnnotations = getTargetMethod(joinPoint).getParameterAnnotations();
+        for (int index = 0; index < arguments.length; index++) {
+            var argument = (P) arguments[index];
+            findAnnotation(argumentsAnnotations[index], annotationType)
+                    .ifPresent(annotation -> task.tryDo(argument, annotation));
+        }
+    }
+
+    public interface Task<P, A> {
+
+        void tryDo(P parameterType, A annotationType);
+
     }
 
 }
