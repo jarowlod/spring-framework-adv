d1366b78d5136bc45f2c424145468cdb7669c887
landrzejewski <landrzejewski.poczta@gmail.com>
2022-12-01 10:40:04 +0100

    kk

---
+++ search/src/main/java/pl/training/search/Main.java
@@ -0,0 +1,14 @@
+package pl.training.search;
+
+public class Main {
+
+    private void start() {
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Main().start();
+        Thread.sleep(100_000);
+    }
+
+}
